d77caa1283ea4dc0e02a0f8a3395ec61e2b8312d
anniejemide <anniejemide@gmail.com>
2021-04-29 04:22:55 +0100

    Updated file

--- PythonCode.py
+++ PythonCode.py
@@ -1,3 +1,5 @@
+import string
+
 # Given an integer, return the integer with reversed digits.
 # Note: The integer could be either positive or negative.
 
@@ -14,12 +16,13 @@ print(rev_int(567))
 
 # For a given sentence, return all words in a sentence in a list.
 
-sentence1 = "Hi all, my name is Tom...I am originally from Australia."
+sentence1 = "Hi all, my name is Annie...I am originally from America."
 sentence2 = "I need to work very hard to learn more about algorithms in Python!"
 
 # Remember to remove punctuation first.
 def  avg_length(sentence):
-    for i in "?!,.'!;:":
+    punctuation = string.punctuation
+    for i in punctuation:
         sentence = sentence.replace(i, '')
         return sentence.split()
 
